38303e8c643e65174febf4d0be244e2f6fd46682
1 <123>
2022-01-23 07:59:00 +0800

    debug compile

--- filters/ChannelCopyTestFilter.cpp
+++ filters/ChannelCopyTestFilter.cpp
@@ -5,32 +5,32 @@
 #include <atltime.h>
 
 #pragma once
-ChannelCopyTestFilter::ChannelCopyTestFilter(){
+ChannelCopyTestFilter::ChannelCopyTestFilter() {
 
 }
 
-ChannelCopyTestFilter::~ChannelCopyTestFilter(){
+ChannelCopyTestFilter::~ChannelCopyTestFilter() {
 
 }
 
-std::vector<std::wstring> ChannelCopyTestFilter::initialize(float sampleRate, unsigned maxFrameCount, std::vector<std::wstring> channelNames){
+std::vector<std::wstring> ChannelCopyTestFilter::initialize(float sampleRate, unsigned maxFrameCount, std::vector<std::wstring> channelNames) {
 	return channelNames;
 }
 
 void ChannelCopyTestFilter::process(float** output, float** input, unsigned frameCount) {
-	if (receiver == NULL){
+	if (receiver == NULL) {
 		return;
 	}
-	double* data = (double *)receiver->unionBuff.data;
+	double* data = (double*)receiver->unionBuff.data;
 	double vol[2];
-	vol[0] = data[1];
-	if (vol[0] <= 0.0001 || vol[0] > 1){
-		vol[0] = 1;
+	vol[0] = data[0];
+	vol[1] = data[1];
+	if (vol[0] + vol[1] <= 0 || vol[0] + vol[1] > 1) {
+		vol[0] = 1, vol[1] = 0;
 	}
-	vol[1] = 1 - vol[0];
-	for (int ch = 0; ch != 2; ++ch){
-		for (int f = 0; f != frameCount; ++f){
-			output[ch][f] = input[ch][f] * vol[ch];
+	for (int ch = 0; ch != 2; ++ch) {
+		for (int f = 0; f != frameCount; ++f) {
+			output[ch][f] = input[0][f] * vol[ch];
 		}
 	}
 }
